3ca40dd6aa9c433407443d23633703e4dd3457be
bootcoder <bootcoder@gmail.com>
2016-06-13 13:52:57 -0700

    create_mentors migration

--- db/migrate/20160613203000_create_mentors.rb
+++ db/migrate/20160613203000_create_mentors.rb
@@ -2,6 +2,24 @@ class CreateMentors < ActiveRecord::Migration
   def change
     create_table :mentors do |t|
 
+      t.string :first_name
+      t.string :last_name
+      t.string :phone
+      t.string :email
+      t.string :img_url
+      t.string :github_handle
+      t.string :cohort
+
+      t.text :address
+      t.text :bio
+
+      t.boolean :is_employed?
+
+      t.date :dbc_start
+      t.date :dbc_end
+
+      t.references :location
+
       t.timestamps null: false
     end
   end
