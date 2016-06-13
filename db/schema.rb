fb54f35a290e97dcfa3c496a47d4af780dbfbc91
bootcoder <bootcoder@gmail.com>
2016-06-13 15:27:06 -0700

    initial seeds

---
+++ db/schema.rb
@@ -0,0 +1,93 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160613205358) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "img_url"
+    t.string   "github_handle"
+    t.integer  "location_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "cost_center"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "employments", force: :cascade do |t|
+    t.string   "company_name"
+    t.date     "start_date"
+    t.integer  "mentor_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "time_zone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mentors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "img_url"
+    t.string   "github_handle"
+    t.string   "cohort"
+    t.text     "address"
+    t.text     "bio"
+    t.boolean  "is_employed?"
+    t.date     "dbc_start"
+    t.date     "dbc_end"
+    t.integer  "location_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "admin_id"
+    t.integer  "mentor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.string   "time_zone"
+    t.integer  "accepts_mentors"
+    t.boolean  "booked?"
+    t.integer  "admin_id"
+    t.integer  "mentor_id"
+    t.integer  "location_id"
+    t.integer  "department_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,49 @@ hunter = Admin.create(first_name: "Hunter",
              img_url: "https://www.dropbox.com/s/sk1e7fg5k8ypza7/20150729_DBC_0046_hi.jpg?dl=0",
              github_handle: "bootcoder",
              location: san_francisco)
+
+uber = Employment.new(company_name: "Uber",
+                      start_date: 10.days.ago)
+
+james = Mentor.create(first_name: "James",
+             last_name: "Bomotti",
+             phone: "415 246-1132",
+             email: "zilliahix@gmail.com",
+             img_url: "https://avatars.githubusercontent.com/u/10890778?v=3",
+             github_handle: "jbomotti",
+             location: san_francisco)
+
+james.employments << uber
+
+ph1 = Department.create(name: "Phase 1",
+                        cost_center: 123456)
+
+shift = Shift.create(time_zone: "UTC -08:00",
+                     accepts_mentors: 2,
+                     booked?: false,
+                     admin: hunter,
+                     location: san_francisco,
+                     department: ph1,
+                     start: 1.day.from_now,
+                     end: 2.days.from_now)
+
+
+shift_to_book = Shift.create(time_zone: "UTC -08:00",
+                     accepts_mentors: 1,
+                     booked?: false,
+                     admin: hunter,
+                     location: san_francisco,
+                     department: ph1,
+                     start: 3.days.from_now,
+                     end: 4.days.from_now)
+
+james.shifts << shift_to_book
+
+
+
+
+
+
+
+
+
